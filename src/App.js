8e4ff92be0a1ba29bf2c27b1a7a811dbc3b8aa14
Emma Rafkin <emmarafkin@gmail.com>
2020-06-04 00:14:12 -0400

    added google analytics"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Form from './Form';
-
+import ReactGA from 'react-ga';
 
 
 function App() {
+  ReactGA.initialize('UA-168571845-1');
+  ReactGA.pageview('/');
   return (
     <div className="App">
       <header className="main">
